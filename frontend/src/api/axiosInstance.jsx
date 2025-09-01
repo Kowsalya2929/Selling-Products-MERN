a927f29241c9bfd82f28164474d74f5c77684879
Kowsalya L <kowsalyal0303@gmail.com>
2025-09-01 21:01:45 +0530

    frontend api changed

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const BASE_URL = import.meta.env.PROD
-  ? "https://selling-products-mern.onrender.com/api" 
-  : "http://localhost:8000/api";
+  ? "http://localhost:8000/api"  
+  : "https://selling-products-mern.onrender.com/api";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
